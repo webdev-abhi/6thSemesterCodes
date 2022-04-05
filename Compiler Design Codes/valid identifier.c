244373c5cc77c86c7b349562eae9332ea19e3923
Abhishek Anand <76611051+abhianand0110@users.noreply.github.com>
2022-04-06 00:11:24 +0530

    Update valid identifier.c

--- Compiler Design Codes/valid identifier.c
+++ Compiler Design Codes/valid identifier.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
 #include<conio.h>
 #include<ctype.h>
-void main()
+
+void main ()
 {
-char a[10];
-int flag, i=1;
-clrscr();
-printf("\n Enter an identifier:");
-gets(a);
-if(isalpha(a[0]))
- flag=1;
-else
- printf("\n Not a valid identifier");
-while(a[i]!='\0')
-{
- if(!isdigit(a[i])&&!isalpha(a[i]))
- {
- flag=0;
- break;
- }
- i++;
+  char a[10];
+  int flag, i = 1;
+  printf ("\n Enter an identifier:");
+  gets (a);
+  if (isalpha (a[0]) || a[0] == '_' )
+    flag = 1;
+  else
+    printf ("\n Not a valid identifier");
+
+
+  while (a[i] != '\0')
+    {
+      if (!isdigit (a[i]) && !isalpha (a[i]) && a[i] != '_')
+	{
+	  flag = 0;
+	  break;
+	}
+      i++;
+    }
+  if (flag == 1)
+    printf ("\n Valid identifier");
+  else
+    printf ("\n Not a valid identifier");
 }
-if(flag==1)
- printf("\n Valid identifier");
-getch();
-}
+
