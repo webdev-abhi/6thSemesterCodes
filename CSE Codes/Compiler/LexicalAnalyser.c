b8fd0b731abf34befcf0be5865c2b20cd90de815
Kumar Avijeet <kumaravijeet108@gmail.com>
2022-05-03 12:44:44 +0530

    Update LexicalAnalyser.c

--- CSE Codes/Compiler/LexicalAnalyser.c
+++ CSE Codes/Compiler/LexicalAnalyser.c
@@ -1,280 +1,211 @@
-#include<stdio.h>
-#include<string.h>
-#define TSIZE 128
-// table[i][j] stores
-// the index of production that must be applied on
-// ith varible if the input is
-// jth nonterminal
-int table[100][TSIZE];
-// stores all list of terminals
-// the ASCII value if use to index terminals
-// terminal[i] = 1 means the character with
-// ASCII value is a terminal
-char terminal[TSIZE];
-// stores all list of terminals
-// only Upper case letters from 'A' to 'Z'
-// can be nonterminals
-// nonterminal[i] means ith alphabet is present as
-// nonterminal is the grammar
-char nonterminal[26];
-// structure to hold each production
-// str[] stores the production
-// len is the length of production
-struct product {
-    char str[100];
-    int len;
-}pro[20];
-// no of productions in form A->�
-int no_pro;
-char first[26][TSIZE];
-char follow[26][TSIZE];
-// stores first of each production in form A->�
-char first_rhs[100][TSIZE];
-// check if the symbol is nonterminal
-int isNT(char c) {
-    return c >= 'A' && c <= 'Z';
+#include <stdio.h>
+#include <malloc.h>
+#include <ctype.h>
+int compare(char *a, char *b)
+{
+int ct = 0;
+while (*(a + ct) != '\0')
+{
+if (((int)*(b + ct) - (int)*(a + ct)) != 0)
+{
+return 0;
 }
-// reading data from the file
-void readFromFile() {
-    FILE* fptr;
-    fptr = fopen("text.txt", "r");
-    char buffer[255];
-    int i;
-    int j;
-    while (fgets(buffer, sizeof(buffer), fptr)) {
-        printf("%s", buffer);
-        j = 0;
-        nonterminal[buffer[0] - 'A'] = 1;
-        for (i = 0; i < strlen(buffer) - 1; ++i) {
-            if (buffer[i] == '|') {
-                ++no_pro;
-                pro[no_pro - 1].str[j] = '\0';
-                pro[no_pro - 1].len = j;
-                pro[no_pro].str[0] = pro[no_pro - 1].str[0];
-                pro[no_pro].str[1] = pro[no_pro - 1].str[1];
-                pro[no_pro].str[2] = pro[no_pro - 1].str[2];
-                j = 3;
-            }
-            else {
-                pro[no_pro].str[j] = buffer[i];
-                ++j;
-                if (!isNT(buffer[i]) && buffer[i] != '-' && buffer[i] != '>') {
-                    terminal[buffer[i]] = 1;
-                }
-            }
-        }
-        pro[no_pro].len = j;
-        ++no_pro;
-    }
+ct++;
 }
-void add_FIRST_A_to_FOLLOW_B(char A, char B) {
-    int i;
-    for (i = 0; i < TSIZE; ++i) {
-        if (i != '^')
-            follow[B - 'A'][i] = follow[B - 'A'][i] || first[A - 'A'][i];
-    }
+return 1;
 }
-void add_FOLLOW_A_to_FOLLOW_B(char A, char B) {
-    int i;
-    for (i = 0; i < TSIZE; ++i) {
-        if (i != '^')
-            follow[B - 'A'][i] = follow[B - 'A'][i] || follow[A - 'A'][i];
-    }
+int checkop(char ch1, char ch2)
+{
+int i = 0;
+char *txt = (char *)malloc(3);
+*txt = ch1;
+*(txt + 1) = ch2;
+*(txt + 2) = '\0';
+char *op[] = {"++", "--", "==", ">=", ">>", "<=", "<<", "&&", "||", "!=", "**",
+"//", "/*", "*/", "::"};
+while (i < 15)
+{
+if (compare(op[i], txt))
+return 1;
+i++;
 }
-void FOLLOW() {
-    int t = 0;
-    int i, j, k, x;
-    while (t++ < no_pro) {
-        for (k = 0; k < 26; ++k) {
-            if (!nonterminal[k])    continue;
-            char nt = k + 'A';
-            for (i = 0; i < no_pro; ++i) {
-                for (j = 3; j < pro[i].len; ++j) {
-                    if (nt == pro[i].str[j]) {
-                        for (x = j + 1; x < pro[i].len; ++x) {
-                            char sc = pro[i].str[x];
-                            if (isNT(sc)) {
-                                add_FIRST_A_to_FOLLOW_B(sc, nt);
-                                if (first[sc - 'A']['^'])
-                                    continue;
-                            }
-                            else {
-                                follow[nt - 'A'][sc] = 1;
-                            }
-                            break;
-                        }
-                        if (x == pro[i].len)
-                            add_FOLLOW_A_to_FOLLOW_B(pro[i].str[0], nt);
-                    }
-                }
-            }
-        }
-    }
+return 0;
 }
-void add_FIRST_A_to_FIRST_B(char A, char B) {
-    int i;
-    for (i = 0; i < TSIZE; ++i) {
-        if (i != '^') {
-            first[B - 'A'][i] = first[A - 'A'][i] || first[B - 'A'][i];
-        }
-    }
+void operate(char *c, FILE *wt, int fg)
+{
+char *temp = c;
+if (compare("#include", temp))
+{
+while (*temp != '\0')
+{
+if (*temp != ' ')
+fprintf(wt, "%c", *(temp));
+temp++;
 }
-void FIRST() {
-    int i, j;
-    int t = 0;
-    while (t < no_pro) {
-        for (i = 0; i < no_pro; ++i) {
-            for (j = 3; j < pro[i].len; ++j) {
-                char sc = pro[i].str[j];
-                if (isNT(sc)) {
-                    add_FIRST_A_to_FIRST_B(sc, pro[i].str[0]);
-                    if (first[sc - 'A']['^'])
-                        continue;
-                }
-                else {
-                    first[pro[i].str[0] - 'A'][sc] = 1;
-                }
-                break;
-            }
-            if (j == pro[i].len)
-                first[pro[i].str[0] - 'A']['^'] = 1;
-        }
-        ++t;
-    }
 }
-void add_FIRST_A_to_FIRST_RHS__B(char A, int B) {
-    int i;
-    for (i = 0; i < TSIZE; ++i) {
-        if (i != '^')
-            first_rhs[B][i] = first[A - 'A'][i] || first_rhs[B][i];
-    }
+else
+{
+while (*temp != '\0')
+{
+char ch = *(temp + 1);
+if (isalnum(*temp) || *temp == '_' || *temp == '.' || *temp == '\"')
+{
+fprintf(wt, "%c", *temp);
+if (!(isalnum(ch) || ch == '_' || ch == '.' || ch == '\"'))
+{
+fprintf(wt, "%c", ' ');
 }
-// Calculates FIRST(�) for each A->�
-void FIRST_RHS() {
-    int i, j;
-    int t = 0;
-    while (t < no_pro) {
-        for (i = 0; i < no_pro; ++i) {
-            for (j = 3; j < pro[i].len; ++j) {
-                char sc = pro[i].str[j];
-                if (isNT(sc)) {
-                    add_FIRST_A_to_FIRST_RHS__B(sc, i);
-                    if (first[sc - 'A']['^'])
-                        continue;
-                }
-                else {
-                    first_rhs[i][sc] = 1;
-                }
-                break;
-            }
-            if (j == pro[i].len)
-                first_rhs[i]['^'] = 1;
-        }
-        ++t;
-    }
 }
-int main() {
-    printf("By Sagnick Bhar \n");
-    readFromFile();
-    follow[pro[0].str[0] - 'A']['$'] = 1;
-    FIRST();
-    FOLLOW();
-    FIRST_RHS();
-    int i, j, k;
-
-    // display first of each variable
-    printf("\n");
-    for (i = 0; i < no_pro; ++i) {
-        if (i == 0 || (pro[i - 1].str[0] != pro[i].str[0])) {
-            char c = pro[i].str[0];
-            printf("FIRST OF %c: ", c);
-            for (j = 0; j < TSIZE; ++j) {
-                if (first[c - 'A'][j]) {
-                    printf("%c ", j);
-                }
-            }
-            printf("\n");
-        }
-    }
-
-    // display follow of each variable
-    printf("\n");
-    for (i = 0; i < no_pro; ++i) {
-        if (i == 0 || (pro[i - 1].str[0] != pro[i].str[0])) {
-            char c = pro[i].str[0];
-            printf("FOLLOW OF %c: ", c);
-            for (j = 0; j < TSIZE; ++j) {
-                if (follow[c - 'A'][j]) {
-                    printf("%c ", j);
-                }
-            }
-            printf("\n");
-        }
-    }
-    // display first of each variable �
-    // in form A->�
-    printf("\n");
-    for (i = 0; i < no_pro; ++i) {
-        printf("FIRST OF %s: ", pro[i].str);
-        for (j = 0; j < TSIZE; ++j) {
-            if (first_rhs[i][j]) {
-                printf("%c ", j);
-            }
-        }
-        printf("\n");
-    }
-
-    // the parse table contains '$'
-    // set terminal['$'] = 1
-    // to include '$' in the parse table
-    terminal['$'] = 1;
-
-    // the parse table do not read '^'
-    // as input
-    // so we set terminal['^'] = 0
-    // to remove '^' from terminals
-    terminal['^'] = 0;
-
-    // printing parse table
-    printf("\n");
-    printf("\n\t**************** LL(1) PARSING TABLE *******************\n");
-    printf("\t--------------------------------------------------------\n");
-    printf("%-10s", "");
-    for (i = 0; i < TSIZE; ++i) {
-        if (terminal[i])    printf("%-10c", i);
-    }
-    printf("\n");
-    int p = 0;
-    for (i = 0; i < no_pro; ++i) {
-        if (i != 0 && (pro[i].str[0] != pro[i - 1].str[0]))
-            p = p + 1;
-        for (j = 0; j < TSIZE; ++j) {
-            if (first_rhs[i][j] && j != '^') {
-                table[p][j] = i + 1;
-            }
-            else if (first_rhs[i]['^']) {
-                for (k = 0; k < TSIZE; ++k) {
-                    if (follow[pro[i].str[0] - 'A'][k]) {
-                        table[p][k] = i + 1;
-                    }
-                }
-            }
-        }
-    }
-    k = 0;
-    for (i = 0; i < no_pro; ++i) {
-        if (i == 0 || (pro[i - 1].str[0] != pro[i].str[0])) {
-            printf("%-10c", pro[i].str[0]);
-            for (j = 0; j < TSIZE; ++j) {
-                if (table[k][j]) {
-                    printf("%-10s", pro[table[k][j] - 1].str);
-                }
-                else if (terminal[j]) {
-                    printf("%-10s", "");
-                }
-            }
-            ++k;
-            printf("\n");
-        }
-    }
+else if (*temp == ',' || *temp == ';' || *temp == ' ')
+{
+fprintf(wt, "%c", ' ');
+}
+else if (checkop(*temp, *(temp + 1)))
+{
+fprintf(wt, "%c%c ", *temp, *(temp + 1));
+temp++;
+}
+else
+{
+fprintf(wt, "%c ", *temp);
+}
+temp++;
+}
+}
+}
+void tokenize(char *str, FILE *ptr)
+{
+char *keyset[] = {"auto", "break", "case", "char", "const", "continue", "default", "do",
+"double", "else", "enum", "extern", "float", "for", "goto", "if",
+"int", "long", "register", "return", "printf", "scanf", "short", "signed", "sizeof", "static",
+"struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"};
+char *opset[] = {"+", "-", "*", "/", "%", "=", ">", "<", "&", "|", "!", "++", "--", "==", ">=",
+">>", "<=", "<<", "&&", "||", "!="};
+int i, class = 1;
+if (*str == '#')
+{
+fprintf(ptr, "%s \t --Preprocessor Directive\n", str);
+return;
+}
+if (*str == '\"')
+{
+fprintf(ptr, "%s \t --Literal\n", str);
+return;
+}
+if (!isalpha(*(str)) && *(str) != '_')
+{
+class = 0;
+}
+if (class)
+{
+for (i = 1; *(str + i) != '\0'; i++)
+{
+if (!isalnum(*(str + i)) && *(str + i) != '_')
+{
+class = 0;
+break;
+}
+}
+}
+if (class)
+{
+for (i = 0; i < 34; i++)
+{
+if (compare(keyset[i], str))
+{
+class = 2;
+break;
+}
+}
+}
+if (!class)
+{
+for (i = 0; i < 21; i++)
+{
+if (compare(str, opset[i]))
+{
+class = 3;
+break;
+}
+}
+}
+if (!class)
+{
+for (i = 0; *(str + i) != '\0'; i++)
+{
+if (isdigit(*(str + i)) || *(str + i) == '.')
+{
+class = 4;
+}
+else
+{
+class = 0;
+break;
+}
+}
+}
+if (!class)
+{
+if (*str == '(' || *str == ')' || *str == '{' || *str == '}' || *str == '[' || *str == ']')
+{
+fprintf(ptr, "%s \t --Separator\n", str);
+return;
+}
+}
+switch (class)
+{
+case 0:
+fprintf(ptr, "%s \t --Not Recognized\n", str);
+break;
+case 1:
+fprintf(ptr, "%s \t --Identifier\n", str);
+break;
+case 2:
+fprintf(ptr, "%s \t --Keyword\n", str);
+break;
+case 3:
+fprintf(ptr, "%s \t --Operator\n", str);
+break;
+case 4:
+fprintf(ptr, "%s \t --Number\n", str);
+}
+}
+void getwd()
+{
+FILE *ptr = fopen("TempStore.txt", "r");
+FILE *wt = fopen("TokenTable.txt", "w");
+char *wd = (char *)malloc(1);
+if (ptr == NULL)
+{
+printf("file not found");
+}
+while (fscanf(ptr, "%s", wd) != EOF)
+{
+tokenize(wd, wt);
+}
+fclose(ptr);
+fclose(wt);
+free(wd);
+}
+int main()
+{
+FILE *ptr = fopen("test.txt", "r");
+FILE *wt = fopen("TempStore.txt", "w");
+char *wd = (char *)malloc(1);
+int flag = 0;
+if (ptr == NULL)
+{
+printf("file not found");
+return 0;
+}
+while (fgets(wd, 100, ptr))
+{
+printf("%s", wd);
+operate(wd, wt, flag);
+}
+fclose(ptr);
+fclose(wt);
+free(wd);
+getwd();
+return 0;
 }
